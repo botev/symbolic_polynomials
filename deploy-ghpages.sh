5b44cb1806f2ee92e9ea49d2d5471c9dd4fc61de
Alexander Botev <botevmg@gmail.com>
2015-06-28 14:10:10 +0100

    Removing echos

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 cargo doc
-echo "Finished docs"
-cd target/doc
+(cd target/doc
 git init
 git config user.name "Botev"
 git config user.email "botevmg@gmail.com"
 cp ../../README.md ./README.md
 cp ../../LICENSE ./LICENSE
 cp ../../index.html ./index.html
-echo "Added all"
 git add .
 git commit -m "Deployed to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages)
