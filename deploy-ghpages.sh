e65268162c342eed577a8108dca78b72aab1d16a
Alexander Botev <botevmg@gmail.com>
2015-06-28 13:57:21 +0100

    Travis with docs

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-cargo build
+cargo doc
+echo "Finished docs"
 ( cd target/doc
  git init
- git config user.name "Travis-CI"
- git config user.email "travis@nodemeatspace.com"
- cp ../CNAME ./CNAME
- cp ../countryiso.js ./countryiso.js
+ git config user.name "Botev"
+ git config user.email "botevmg@gmail.com"
+ cp ../../README.md ./README.md
+ cp ../../LICENSE ./LICENSE
+ cp ../../index.html ./index.html
+ echo "Added all"
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
